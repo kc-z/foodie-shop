596fb5056e4bfee4eeec0a0bb10f888b82d15085
[zkc168] <[467805767@qq.com]>
2020-01-18 14:21:44 +0800

    monify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,7 @@ killTomcat()
     if [ "$pid" = "" ]
     then
       echo "no tomcat pid alive"
+      echo "kill tomcat成功"
     else
       kill -9 $pid
     fi
@@ -30,13 +31,14 @@ killTomcat
 
 # 删除原有工程
 #rm -rf $TOMCAT_APP_PATH/webapps/ROOT
-rm -rf $TOMCAT_APP_PATH/webapps/foodie-shop
 
+rm -rf $TOMCAT_APP_PATH/webapps/foodie-shop
+echo "清除tomcat旧工程成功"
 #前端文件先注释以下代码end
 
 # 复制新的工程
-cp $PROJ_PATH/foodie-shop $TOMCAT_APP_PATH/webapps/
-echo "项目路径:"+$PROJ_PATH/foodie-shop
+cp -r $PROJ_PATH/foodie-shop $TOMCAT_APP_PATH/webapps/
+echo "拷贝项目到tomcat下成功"+$PROJ_PATH/foodie-shop
 #cp $PROJ_PATH/order/foodie-center $TOMCAT_APP_PATH/webapps/ order与项目名对应，jenkins后台会设置项目名
 
 cd $TOMCAT_APP_PATH/webapps/
@@ -45,6 +47,7 @@ cd $TOMCAT_APP_PATH/webapps/
 cd $TOMCAT_APP_PATH/
 echo "tomcat path:"+$TOMCAT_APP_PATH/bin/startup.sh
 sh $TOMCAT_APP_PATH/bin/startup.sh
+echo "重启tomcat成功"
 
 
 
